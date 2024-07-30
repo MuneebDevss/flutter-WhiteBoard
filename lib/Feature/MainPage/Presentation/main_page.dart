d856ced0d98729f3d2b83bc72f71718df8d7b2f1
Muneeb Ur Rehman <i222702@nu.edu.pk>
2024-07-31 03:15:54 +0500

    Created Shape selecter

--- lib/Feature/MainPage/Presentation/main_page.dart
+++ lib/Feature/MainPage/Presentation/main_page.dart
@@ -18,9 +18,7 @@ class _MainPageState extends State<MainPage> {
     double screenHeight = TDeviceUtils.getScreenHeight(context);
     return Scaffold(
       appBar: AppBar(),
-      drawer: Drawer(
-
-      ),
+      drawer: Drawer(),
       body: SizedBox(
         width: double.infinity,
         child: Column(
@@ -28,27 +26,105 @@ class _MainPageState extends State<MainPage> {
             Container(
               alignment: Alignment.center,
               width: screenWidth - (screenWidth / 4),
-              height: screenHeight - (screenHeight / 1.1),
-              decoration:  BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: TColors.grey,
-              ),
-              child: Row(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                      blurRadius: 5,
+                      color: TColors.grey,
+                      blurStyle: BlurStyle.outer,
+                      offset: Offset(0, 1),
+                    )
+                  ]),
+              child: Wrap(
+                spacing: 12,
                 children: [
-                  SizedBox(
-                    height: 50,
-                    width: 50,
-                    child: IconButton(
-                      icon: const Icon(Icons.square),
+                  ShapeSelector(
+                    screenHeight: screenHeight,
+                    button: IconButton(
+                      icon: const Icon(Icons.square_outlined),
                       onPressed: () {},
                     ),
-                  )
+                  ),
+                  ShapeSelector(
+                    screenHeight: screenHeight,
+                    button: IconButton(
+                      icon: const Icon(Icons.circle_outlined),
+                      onPressed: () {},
+                    ),
+                  ),
+                  ShapeSelector(
+                    screenHeight: screenHeight,
+                    button: IconButton(
+                      icon: const Icon(Icons.arrow_forward),
+                      onPressed: () {},
+                    ),
+                  ),
+                  ShapeSelector(
+                    screenHeight: screenHeight,
+                    button: IconButton(
+                      icon: const Text(
+                        '___',
+                        textAlign: TextAlign.center,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  ShapeSelector(
+                    color: TColors.primary,
+                    screenHeight: screenHeight,
+                    button: IconButton(
+                      icon: const Icon(Icons.square_outlined),
+                      onPressed: () {},
+                    ),
+                  ),
+                  ShapeSelector(
+                    screenHeight: screenHeight,
+                    button: IconButton(
+                      icon: const Icon(Icons.square_outlined),
+                      onPressed: () {},
+                    ),
+                  ),
+                  ShapeSelector(
+                    screenHeight: screenHeight,
+                    button: IconButton(
+                      icon: const Icon(Icons.square_outlined),
+                      onPressed: () {},
+                    ),
+                  ),
+                  ShapeSelector(
+                    screenHeight: screenHeight,
+                    button: IconButton(
+                      icon: const Icon(Icons.square_outlined),
+                      onPressed: () {},
+                    ),
+                  ),
                 ],
               ),
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class ShapeSelector extends StatelessWidget {
+  const ShapeSelector({
+    super.key,
+    required this.screenHeight,
+    required this.button, this.color,
+  });
+  final Color? color;
+  final double screenHeight;
+  final IconButton button;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      height: screenHeight - (screenHeight / 1.1),
+      width: 50,
+      child: button,
     );
   }
 }
